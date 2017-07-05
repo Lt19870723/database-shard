73cd75e981d0134e53e235262aac565d2d79d780
master-alen <nchufujianjian@126.com>
2017-07-05 15:20:40 +0800

    数据库分库分表项目小样 系统结构初始化

---
+++ src/main/java/com/nchu/tech/database/DatabaseShardApplication.java
@@ -0,0 +1,12 @@
+package com.nchu.tech.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseShardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseShardApplication.class, args);
+	}
+}
